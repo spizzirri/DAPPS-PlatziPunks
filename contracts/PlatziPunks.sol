026f112673d899ba57207426d2cf81291d685518
damian spizzirri <damianspizzirri@gmail.com>
2022-01-09 18:33:38 -0300

    clase 15

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol";
 
 //https://docs.openzeppelin.com/contracts/4.x/erc721
 
@@ -23,6 +24,21 @@ contract PlatziPunks is ERC721, ERC721Enumerable{
         uint256 current = _idCounter.current();
         _idCounter.increment();
         _safeMint(msg.sender, current);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory){
+        require(_exists(tokenId), "ERC721 Metadata: URI query nonexistent token");
+
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "PlatziPunks #" }',
+                tokenId,
+                '", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
+                "//TODO: Calculate image URL",
+                '"}'
+            )
+        );
+        return string(abi.encodePacked("data:application/json;base64,", jsonURI));
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
